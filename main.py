f1257a89992a25308fa6ff0c2ec111c71d7b7483
Jeff Cooper <jeffreyleecooper@gmail.com>
2013-05-18 16:59:37 -0400

    Designed and added base.html and bootstrap libraries

--- main.py
+++ main.py
@@ -15,10 +15,18 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__),'templates')))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = {
+        }
+    
+        template = JINJA_ENVIRONMENT.get_template('base.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
